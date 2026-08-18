8eb39c3aef64232f698fdcb36cc4abc1e59db4aa
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-08-18 21:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (83.30%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size())
+         return false;
+
+        vector<int> mapST(256, -1);
+        vector<int> mapTS(256, -1);
+
+        for(int i =0; i<s.size(); i++){
+            char a= s[i];
+            char b = t[i];
+
+            if(mapST[a] != -1 && mapST[a] != b)
+                return false;
+
+            if(mapTS[b] != -1 && mapTS[b] != a)
+                return false;
+
+            mapST[a]=b;
+            mapTS[b]=a;
+        }
+        return true;
+    }
+};
